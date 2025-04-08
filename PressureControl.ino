79316771cc87206c3b152423a61b5264d8fca435
Andy Tran <u1301465@gcloud.utah.edu>
2025-04-08 16:23:50 -0600

    Rough Skeleton code for pressure control

---
+++ PressureControl.ino
@@ -0,0 +1,56 @@
+void set_pressure(int target_pressure);
+int get_pressure();
+void update();
+void start_brew(int mode);
+void set_maxpressure(int pressure);
+void set_preinfusetime(int time);
+
+int max_pressure = 172;
+int pre_infuse_time;
+
+
+void set_pressure(int target_pressure) {
+  if (target_pressure >= max_pressure) {
+    target_pressure = max_pressure;
+  }
+  
+}
+int get_pressure() {
+  return calculatePressure();
+
+}
+void update() {
+
+}
+void start_brew(int mode) {
+  switch(mode) {
+    case 1:
+      //statement 1
+      break;
+    case 2:
+      // do something
+      break;
+    default: 
+      // do nothing
+      break;
+  }
+
+}
+void set_maxpressure(int max_pressure) {
+  this.maxpressure = max_pressure;
+
+}
+void set_preinfusetime(int time) {
+  pre_infuse_time = time;
+}
+
+void setup() {
+  // put your setup code here, to run once:
+     Serial.begin(115200);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  Serial.println(get_pressure());
+
+}
