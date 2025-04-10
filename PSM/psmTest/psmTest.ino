b3f73bf76d2310b17874e167eee8016405f841ee
cabobalot <casey8wolfe@gmail.com>
2025-04-10 14:35:36 -0600

    move to library

--- PSM/psmTest/psmTest.ino
+++ PSM/psmTest/psmTest.ino
@@ -1,86 +1,19 @@
+#include "psm.h"
+
 const byte valuePin = 32; // potentiometer input
 const byte controlPin = 33; // triac output
 const byte interruptPin = 25; // phase sensing
 
-const unsigned int range = 127;
-const unsigned int valueFactor = 4; // precalculated constant 1024 [10 bit ADC resolution] / 128 [range + 1] / 2
 
-volatile bool zeroCross = false;
-
-unsigned int intCount = 0;
-
-unsigned long updateDelay = 500;
+unsigned long updateDelay = 100;
 unsigned long lastUpdateTime = 0;
 
 unsigned int value = 0;
 
-unsigned int a = 0;
-bool skip = false;
+psm dimmer(controlPin, interruptPin);
 
 void setup() {
-  pinMode(controlPin, OUTPUT);
-  pinMode(interruptPin, INPUT_PULLUP);
-  attachInterrupt(digitalPinToInterrupt(interruptPin), zeroCrossISR, RISING); // low-to-high transition = start of negative half-wave
-  // EDIT actually we have no idea if its the positive or negative pulse. the pin goes high briefly at the start of both
-
   Serial.begin(115200);
-}
-
-IRAM_ATTR void zeroCrossISR() {
-  zeroCross = true;
-}
-
-void debounceZeroCross() {
-  static unsigned long firedTime;
-  if (millis() < firedTime + 5) {
-    zeroCross = false;
-    return;
-  }
-
-  if (zeroCross) {
-    firedTime = millis();
-    intCount++;
-    calculateSkip();
-    zeroCross = false;
-  }
-}
-
-void calculateSkip() {
-  static bool wholeWaveFlag = false;
-
-  if (wholeWaveFlag) {
-    wholeWaveFlag = false;
-    return;
-  }
-  else {
-    wholeWaveFlag = true;
-
-    a += value;
-
-    if (a >= range) {
-      // a -= range;
-      a = 0;
-      skip = false;
-    } else {
-      skip = true;
-    }
-
-    // if (a > range) {
-    //   a = 0;
-    //   skip = false;
-    // }
-
-    // updateControl();
-  }
-  
-}
-
-void updateControl() {
-  if (skip) {
-    digitalWrite(controlPin, LOW);
-  } else {
-    digitalWrite(controlPin, HIGH);
-  }
 }
 
 void updateValue() {
@@ -90,7 +23,7 @@ void updateValue() {
 
   if (readValue > (oldValue + valueFactor * 3) || (readValue + valueFactor) < oldValue) { // add some hysteresis to filter out noise
     value = readValue / valueFactor / 2;
-    a = 0;
+    dimmer.setValue(value);
   }
 }
 
@@ -121,8 +54,7 @@ void loop() {
     lastUpdateTime = time;
   }
   
-  debounceZeroCross();
-  updateControl();
+  dimmer.update();
 
 }
 
