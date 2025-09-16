52ebd4d30a52c6dcf883fd20e66d21ef4e8cd863
MrFangao <143060549+MrFangao@users.noreply.github.com>
2025-09-16 12:18:24 -0700

    Controller combine to main

--- EspressoMachine/EspressoMachine.ino
+++ EspressoMachine/EspressoMachine.ino
@@ -1,37 +1,52 @@
+#include "Menu.h"
+#include "bitmaps.h"
+#include "pressure_sensor.h"
+#include "temp_sensor.h"
 #include "pins.h"
+#include "pressure_control.h" 
+#include "tempControl.h" 
 
-#include "psm.hpp"
-#include "tempControl.h"
-#include "pressureControl.h"
 
+TemperatureSensor tempSensor;
+PressureControl pc(1.1, 0.1, 0.1);
 
 void setup() {
-  psm::config(PIN_DIMMER_CONTROL, PIN_DIMMER_ZERO_CROSS);
   Serial.begin(115200);
 
-  tempControl::init();
-}
+  Wire.begin(PIN_SCREEN_SDA, PIN_SCREEN_SCL);
+  Wire.setClock(400000); 
+  if (!menu.begin()) {
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;);
+  }
+  //temp
+  tempControl::init(); 
+  //presure
+  pinMode(PIN_PRESSURE_SENSE, INPUT);
+  pc.init(PIN_DIMMER_CONTROL, PIN_DIMMER_ZERO_CROSS);
+  //temporary init
+  pc.setSetpoint(40.0);
 
+  pc.setCurrentPressure(20.0);
+  menu.beginInput(PIN_KNOB_ROTATE_A, PIN_KNOB_ROTATE_B, PIN_KNOB_BUTTON);
+}
 
 void loop() {
-	// Update temperature and setpoint
-	UI.setDisplayTemp(tempControl::getTemperature());
-	tempControl::setTemperature(UI.getTempSetpoint());
+  menu.pollInput();
+  menu.show();
 
-	// Update pressure and setpoint
-	UI.setDisplayPresure(pressureControl::getPressure());
-	pressureControl::setPressure(UI.getPressureSetpoint());
-	pressureControl::setMaxPressure(UI.getMaxPressureSetpoint());
-	pressureControl::setPreinfuseTime(UI.getPreinfuseTime());
+  int step = menu.consumeStep();    
+  if (step != 0) menu.moveSelection(step > 0);
+  if (menu.consumeClick()) menu.select();
+  //temperature 
+  float spC = menu.getTargetTemperature();
+  tempControl::setSetpoint(spC);
+  //pressure
+  double sp = (double)menu.getTargetPressure();
+  pc.setSetpoint(sp);
 
-	// update everything
-	tempControl::update();
-	pressureControl::update();
-	UI.update();
-	PSM::update();
+  pc.update();
+  menu.setCurrentTemperature(tempControl::getTemperature());  
+  menu.setCurrentPressure(pc.getPressure());
 
 }
-
-
-
-
