58b5e57df5f09008b80a5e246adca0043b76b958
Andy Tran <ninja303xd@gmail.com>
2025-11-13 21:32:58 -0700

    Added comments for temp control

--- EspressoMachine/tempControl.cpp
+++ EspressoMachine/tempControl.cpp
@@ -5,22 +5,26 @@
 #include "soc/mcpwm_periph.h"
 #include "pins.h"
 
-
-#define PWM_FREQ 1
+#define PWM_FREQ 1 // Frequency of PWM pin (1Hz)
 
 // Static variables to hold current temp and target
 static double currentTemp = 0.0;
 static double setpoint = 0.0;
 static double outputDuty= 0.0;
 
+// PID varibles
 static double Kp = 7.0, Ki = 1.5, Kd = 4.0;
 
+// Delay between updates (1 sec)
 unsigned long prevMillis = 0;
 const unsigned long interval = 1000;
 
 // PID object from library
 static PID tempPID(&currentTemp, &outputDuty, &setpoint, Kp, Ki, Kd, DIRECT);
 
+/*
+Setup to allow for the temperature control to change the temperature
+*/
 void tempControl::init() {
 
   // PID setup
@@ -30,6 +34,7 @@ void tempControl::init() {
   // PWM setup
   mcpwm_timer_set_resolution(MCPWM_UNIT_0, MCPWM_TIMER_1, 60000);
 
+  // Config to setup the PWM for the ESP32
   mcpwm_gpio_init(MCPWM_UNIT_0, MCPWM1A, PIN_HEATER);
   mcpwm_config_t pwm_config;
   pwm_config.frequency = PWM_FREQ;
@@ -37,55 +42,61 @@ void tempControl::init() {
   pwm_config.counter_mode = MCPWM_UP_COUNTER;
   pwm_config.duty_mode = MCPWM_DUTY_MODE_0;
   mcpwm_init(MCPWM_UNIT_0, MCPWM_TIMER_1, &pwm_config);
-
-  Serial.println(mcpwm_get_frequency(MCPWM_UNIT_0, MCPWM_TIMER_1));
 }
 
+/*
+Setter for the desired setpoint
+*/
 void tempControl::setSetpoint(float temp){
   setpoint = temp;
 }
 
+/*
+Setter for the current temperature
+*/ 
 void tempControl::setCurrentTemp(float temp){
   currentTemp = temp;
 }
 
+/*
+Controls and updates the duty cycle to reach the setpoint
+*/
 void tempControl::setTemperature(float tempSetpoint) {
-
-  //PID Controller  
+  // PID controller
   if(tempPID.Compute()) {
-    // Change Duty Cycle
+    // Changes the duty cycle
     mcpwm_set_duty(MCPWM_UNIT_0, MCPWM_TIMER_1, MCPWM_OPR_A, outputDuty);
   }
-
 }
 
+/*
+Getter for temperature
+*/
 float tempControl::getTemperature() {
   return currentTemp;
 }
 
+/*
+Getter for setpoint
+*/ 
 float tempControl::getSetpoint(){
   return setpoint;
 }
 
-
+/*
+At given intervals gets the current temperature and updates it until the 
+set temperature is reached.
+*/
 void tempControl::update() {
 
     unsigned long currMillis = millis();
 
-    if(currMillis - prevMillis >= interval) {
+    if(currMillis - prevMillis >= interval) { // Delay 
       prevMillis = currMillis;
 
-      currentTemp = TemperatureSensor::getTemperature(); // grab current temp
+      currentTemp = TemperatureSensor::getTemperature(); // Grab the current temperature
       
-      tempControl::setTemperature(setpoint); // reach that temperature
-
-      //Test code
-    //   Serial.print("CurrentTemp:");
-    //   Serial.print(currentTemp, 2);
-    //   Serial.print(",Setpoint:");
-    //   Serial.print(setpoint, 2);
-    //   Serial.print(",PWMOutput:");
-    //   Serial.println(outputDuty, 2);
+      tempControl::setTemperature(setpoint); // Reach that temperature
 
     }
 }
--- EspressoMachine/tempControl.h
+++ EspressoMachine/tempControl.h
@@ -3,23 +3,33 @@
 
 #include "temp_sensor.h"
 
+/*
+Handles and regulates the temperature of the machine.
+*/
 class tempControl {
 private:
 
 public:
 
+	// Initializes all parameters for temperature control.
 	static void init();
 
+	// Sets the setpoint of the machine.
 	static void setSetpoint(float temp);
 
+	// Sets the current temperature of the machine.
 	static void setCurrentTemp(float temp);
 	
+	// Gets the current setpoint
 	static float getSetpoint();
 
+	// Sets the current temperature
 	static void setTemperature(float tempSetpoint);
 
+	// Gets the current temperature
 	static float getTemperature();
 
+	// Controls the output of the relay
 	static void update();
 
 };
